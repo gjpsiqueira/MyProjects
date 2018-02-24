9117323580435fc8c0070f8044d113c37d9bdeca
= <gjpsiqueira@gmail.com>
2018-02-23 22:00:38 -0300

    Board state loop to Note

--- bulletin-board/src/Board.js
+++ bulletin-board/src/Board.js
@@ -3,12 +3,50 @@ import Note from './Note'
 
 class Board extends Component {
 
+	constructor(props) {
+
+		super(props)
+		this.state = {
+			notes: [
+
+				{
+
+					id: 33,
+					note: "Learning C#"
+
+				},
+
+				{
+
+					id: 34,
+					note: "Learning Java"
+				},
+				{
+
+					id: 35,
+					note: "Learning Python"
+				}
+
+			]
+		}
+
+		this.eachNote = this.eachNote.bind(this)
+
+	}
+
+	eachNote(note,i) {
+
+		return (
+			<Note key={i} index={i}>{note.note}</Note>
+		)
+	}
+
 	render() {
 
 		return (
 
 			<div className="board">
-				<Note />
+				{this.state.notes.map(this.eachNote)}
 			</div>
 		)
 	}
