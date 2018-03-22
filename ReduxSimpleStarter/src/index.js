7ccacb22cb0488d6a9c09add61bafe95b9a61b27
= <gjpsiqueira@gmail.com>
2018-03-22 20:07:45 -0300

    add search_bar

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
-const App = function() {
-  return <div>Estou testando...</div>
+const API_KEY = 'AIzaSyAKAf77VTwXiAIeLmxfzVkqYgxAMPu7Zo8'
+
+YTSearch({key: API_KEY, term: 'porta dos fundos'}, function(data) {
+  console.log(data)
+})
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {videos: []};
+  }
+  render() {
+    return (
+      <div><SearchBar /></div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
