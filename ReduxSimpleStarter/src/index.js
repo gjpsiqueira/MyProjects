d96ebcbfaf2094f4c64d08870d3874c5f0af222d
= <gjpsiqueira@gmail.com>
2018-03-23 20:09:16 -0300

    Add list videos and Search Youtube API

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyAKAf77VTwXiAIeLmxfzVkqYgxAMPu7Zo8'
 
-YTSearch({key: API_KEY, term: 'porta dos fundos'}, function(data) {
-  console.log(data)
-})
+
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'Google Developers'}, (videos) => {
+      console.log(videos);
+      this.setState({ videos: videos });
+    });
   }
+
   render() {
     return (
-      <div><SearchBar /></div>
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
     );
   }
 }
