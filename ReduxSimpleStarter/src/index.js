4e93f201927275c37af5fb6764211a5518ecaa5c
= <gjpsiqueira@gmail.com>
2018-03-23 22:14:14 -0300

    Add video select

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -13,11 +13,18 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+
+    };
 
     YTSearch({key: API_KEY, term: 'Google Developers'}, (videos) => {
       console.log(videos);
-      this.setState({ videos: videos });
+      this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
     });
   }
 
@@ -25,8 +32,10 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
